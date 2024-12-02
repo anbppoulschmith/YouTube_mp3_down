cefecc985d0029f4f6fb6879c20c37cedaa2dcd3
anbppoulschmith <anbp@poulschmith.dk>
2024-12-02 16:29:22 +0100

    Download updates mp3

--- src/mp3_convert.py
+++ src/mp3_convert.py
@@ -12,16 +12,24 @@ def mp3_convert():
     with col2:
         youtube_link = st.text_input("Youtube link")
 
+        if not youtube_link:
+            st.warning("Please enter a YouTube link.")
+            return
+        
         if st.button('Convert'):
-            mp3_data, file_name = download_songs_streamlit(youtube_link)
+            try:
+                
+                mp3_data, file_name = download_songs_streamlit(youtube_link)
 
-            if mp3_data:
-                st.success("Conversion successful! Click to donwload below")
-                st.download_button(
-                    label="Download mp3",
-                    data=mp3_data,
-                    file_name=os.path.basename(file_name),
-                    mime="audio/mpeg"
-                )
-            else:
-                st.error("Failed to download or convert the Youtube link")
+                if mp3_data and file_name:
+                    st.success("Conversion successful! Click to donwload below")
+                    st.download_button(
+                        label="Download mp3",
+                        data=mp3_data,
+                        file_name=file_name,
+                        mime="audio/mpeg"
+                    )
+                else:
+                    st.error("Failed to download or convert the Youtube link")
+            except Exception as e:
+                st.error("An error has ocurred: {e}")
